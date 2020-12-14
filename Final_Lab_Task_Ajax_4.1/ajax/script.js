131163becac6b83a2b081733ecba7ca67827085a
unknown <muniamishu600@gmail.com>
2020-12-14 10:06:33 +0600

    ajax

---
+++ Final_Lab_Task_Ajax_4.1/ajax/script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+function registration(){
+	
+    let id = document.getElementsByName('id').value;
+    let password = document.getElementsByName('password').value;
+    let changepassword = document.getElementsByName('changepassword').value;
+	let name= document.getElementsByName('name').value;
+    let email = document.getElementsByName('email').value;
+    let usertype = document.getElementsByName('usertype').value;
+    
+}
