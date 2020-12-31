d654b7223bb4c3fe5101c17a3d652f2eee9c0d7b
unknown <muniamishu600@gmail.com>
2020-12-31 23:30:55 +0600

    sql

--- Final_Lab_Task_Ajax_4.1/models/userService.php
+++ Final_Lab_Task_Ajax_4.1/models/userService.php
@@ -6,7 +6,7 @@ require_once('db.php');
 function userRegistration($id, $password, $confirmpassword, $email){
     
     $conn = getConnection();
-    $sql = "insert into employee (id, password, confirmpassword, email) values ('$id', '$password', '$confirmpassword', '$email')";
+    $sql = "insert into user (id, password, confirm-password,name, email, user-type) values ('$id', '$password', '$confirm_Spassword','$name', '$email','$user_Stype')";
     
     if(mysqli_query($conn, $sql)){
         
@@ -24,7 +24,7 @@ function userRegistration($id, $password, $confirmpassword, $email){
 function validateUser($userid, $password){
     
     $conn = getConnection();
-    $sql = "select * from employee where username = '$userid' and password = '$password'";
+    $sql = "select * from user where username = '$userid' and password = '$password'";
     
     if(mysqli_query($conn, $sql)){
         
